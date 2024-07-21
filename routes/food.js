2828f5abfa22be16bf182ae4abb0e98526081cd7
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-07-21 05:55:26 +0100

    cr food

--- routes/food.js
+++ routes/food.js
@@ -5,10 +5,28 @@ const Food = require("../models/Food")
 const Ingredient = require("../models/Ingredient")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	const allIngredient = Ingredient.find({})
+router.get('/', async function(req, res, next) {
+	const allIngredient = await Ingredient.find({})
 
-  res.render('food_form', { title: 'Add Food', allIngredient });
+  res.render('food_form', { 
+		title: 'Add Food', 
+		allIngredient 
+	});
 });
+
+router.post("/", async function(req, res, next){
+	const content = req.body
+	
+	const newFood = new Food({
+		FoodName: content["food_name"],
+		isLimited: content["is_limited"],
+		FoodPrice: content["food_price"],
+		TimeTakes: content["time_takes"],
+	})
+
+	await newFood.save()
+
+	res.redirect("/")
+})
 
 module.exports = router;
