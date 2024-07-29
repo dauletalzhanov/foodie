a5812f556f1251360f49ff785b6aba42ca328763
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-07-29 17:38:00 +0100

    react - food list

--- react/src/Pages/Shop.jsx
+++ react/src/Pages/Shop.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react"
 import { Helmet } from "react-helmet"
+import { Link, useNavigate } from "react-router-dom"
 
 import "./shop.css"
 
 export default function Shop(){
 	const [menu, setMenu] = useState([])
+	const [balance, setBalance] = useState(0)
+
+	const navigate = useNavigate()
+
+	const menu_id = `66a7c2d8945da678d761f0b8`
 
 	useEffect(()=>{
 		async function getFood(restaurant_id){
@@ -18,31 +24,58 @@ export default function Shop(){
 						return response.json()
 				})
 
-			
-
 			setMenu(res)
 			//console.log(menu)
 		}
 
-		getFood("669cfe05e9ca72abe25b5c2f")
+		getFood(menu_id)
 	}, [menu])
+
+	function addButton(event){
+		const food = event.target.parentNode.id
+		console.log(menu[food])
+
+		setBalance(i => i + menu[food]["FoodPrice"])
+	}
+
+	function orderItems(){
+		navigate("/items")
+	}
 
 	return(<>
 		<h1>Shop</h1>
 
 		<div className="catalogue">
-			{menu.map((food) => {
-				return(
-					<p key={food["_id"]}>{food["FoodName"]} - {food["FoodPrice"]}</p>
+			{menu.map((food, index) => {
+				return(<div className="catalogue-item" id={index} key={index}>
+						<h3>{food["FoodName"]}</h3>
+						<div className="pizza-red"></div>
+						<p className="food-description" >
+							${food["FoodPrice"]} - {food["TimeTakes"]}Mins
+						</p>
+						<p><strong>Ingredients: </strong>{food["IngredientsAvailable"].map((i) => i)}</p>
 
-				)
+						<button onClick={addButton}>ADD</button>
+					</div>)
 			})}
+		</div>
+
+		<div className="total-product">
+			<div className="leftie">
+				<p>Total - ${balance}</p>
+
+				<Link to="/items">List of Items</Link>
+			</div>
+
+			<button onClick={orderItems}>Proceed to Order</button>
+
 		</div>
 
 
 		<Helmet>
 			<title>Foodie - Shopping</title>
 			<meta name="description" content="Food Catalogue" />
+			<meta name="keywords" content={menu.map((food)=> food["FoodName"])} />
 		</Helmet>
 	</>)
 }
