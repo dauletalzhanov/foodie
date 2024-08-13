5a6e94e803b506cf2bd966b78a05e74bb40c2c6a
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-08-13 14:00:17 +0100

    food crud

--- routes/food.js
+++ routes/food.js
@@ -5,7 +5,11 @@ const Food = require("../models/Food")
 const Ingredient = require("../models/Ingredient")
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get("/", async function(req, res, next){
+	res.redirect("/food/all")
+})
+
+router.get('/add', async function(req, res, next) {
 	const allIngredient = await Ingredient.find({})
 
   res.render('food_form', { 
@@ -14,7 +18,7 @@ router.get('/', async function(req, res, next) {
 	});
 });
 
-router.post("/", async function(req, res, next){
+router.post("/add", async function(req, res, next){
 	const content = req.body
 	
 	const newFood = new Food({
@@ -29,10 +33,62 @@ router.post("/", async function(req, res, next){
 	res.redirect("/")
 })
 
+////////////////////////////////////////////////////
 router.get("/all", async function(req, res, next){
 	const foods = await Food.find({})
 
 	res.render("food_all", { title: "All Food", foods })
 })
+
+//////				DELETE
+router.get("/delete/:id", async function(req, res, next){
+	const id = req.params.id
+	const food = await Food.findById(id)
+
+	res.render("food_delete", {
+		title: `Delete: ${food.FoodName}`
+	})
+})
+
+router.post("/delete/:id", async function(req, res, next){
+	const id = req.params.id
+	const food = await Food.findByIdAndDelete(id)
+
+	res.redirect("/food/")
+})
+
+///// UPDATE
+router.get("/update/:id", async function(req, res, next){
+	const id = req.params.id
+	const food = await Food.findById(id)
+	const allIngredient = await Ingredient.find({})
+
+	res.render("food_form", {
+		title: `Update: ${food.FoodName}`,
+
+		food,
+		allIngredient
+	})
+})
+
+router.post("/update/:id", async function(req, res, next){
+	const id = req.params.id
+	const body = req.body
+
+	console.log(body)
+
+	const content = {
+		FoodName: 							body["food_name"],
+		IngredientsAvailable: 	body["ingredient_list"],
+		isLimited: 							body["is_limited"],
+		FoodPrice: 							body["food_price"],
+		TimeTakes: 							body["time_takes"]
+	}
+
+	await Food.findByIdAndUpdate(id, content)
+
+	res.redirect("/food/")
+})
+
 
 module.exports = router;
