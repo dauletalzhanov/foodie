ff155195bbd865a847b831ce7ba0a5b017631ef0
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-09-02 18:01:23 +0100

    signout

--- react/src/Pages/Signout.jsx
+++ react/src/Pages/Signout.jsx
@@ -1,11 +1,36 @@
-
+import { useCookies } from "react-cookie"
+import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet"
 
 export default function Signout(){
+	const [cookies, setCookie] = useCookies(['identifier']);
+	const navigate = useNavigate()
+
+	function logout(){
+		setCookie("identifier", "")
+		navigate("/")
+	}
+
+	function leave(){
+		navigate("/shop/")
+	}
 
 	return(<>
-		<h1>Log Out</h1>
+		<div className="logout" >
+			<h1> Are you sure you want to log out? </h1>
 
-		
-	
+			<div className="buttons" >
+				<button onClick={logout}> Yes </button>
+				<button onClick={leave} >  No </button>
+			</div>
+			
+		</div>
+
+		<Helmet>
+			<title> Log Out </title>
+			<meta name="description" content="" />
+			<meta name="keywords" content="" />
+			<link rel="stylesheet" href="../public/signout.css" />
+		</Helmet>
 	</>)
 }
